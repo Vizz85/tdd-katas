f608dfb5e08eb655b42f3251d859f6e656d26bef
Alessandro Vizin <alessandro.vizin@valtech.de>
2019-05-12 12:21:17 +0200

    refactoring

--- src/expandedForm.js
+++ src/expandedForm.js
@@ -6,6 +6,23 @@ function expandedFormOfSingleDigit(digit, positionOfDigitFromRight) {
 	return digit * Math.pow(10, positionOfDigitFromRight);
 }
 
+function digitShouldAppearInResultString(digit) {
+	return digit != 0;
+}
+
+function handleSingleDigit(digit, positionOfDigitFromRight) {
+	let outputOfSingleDigit = '';
+
+	if (digitShouldAppearInResultString(digit)) {
+		outputOfSingleDigit += expandedFormOfSingleDigit(digit, positionOfDigitFromRight);
+
+		if (shouldAddPlusSign(positionOfDigitFromRight)) {
+			outputOfSingleDigit += ' + ';
+		}
+	}
+	return outputOfSingleDigit;
+}
+
 const expandedForm = (input) => {
 	const arrayOfDigits = ('' + input).split('');
 	let resultString = '';
@@ -13,16 +30,7 @@ const expandedForm = (input) => {
 	for (let i = 0; i < arrayOfDigits.length; i++) {
 		const digit = arrayOfDigits[i];
 		const positionOfDigitFromRight = arrayOfDigits.length - 1 - i;
-
-		if (digit == 0) {
-			continue;
-		}
-
-		resultString += expandedFormOfSingleDigit(digit, positionOfDigitFromRight);
-
-		if (shouldAddPlusSign(positionOfDigitFromRight)) {
-			resultString += ' + ';
-		}
+		resultString += handleSingleDigit(digit, positionOfDigitFromRight);
 	}
 
 	return resultString;
