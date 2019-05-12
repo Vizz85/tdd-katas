75545a955d0f4dffbd751b3e8e7d3318e8938896
Alessandro Vizin <alessandro.vizin@valtech.de>
2019-05-12 11:58:27 +0200

    refactoring - extract methods and variables

--- src/expandedForm.js
+++ src/expandedForm.js
@@ -2,15 +2,19 @@ function shouldAddPlusSign(positionOfDigitFromRight) {
 	return positionOfDigitFromRight > 0;
 }
 
+function expandedFormOfSingleDigit(digit, positionOfDigitFromRight) {
+	return digit * Math.pow(10, positionOfDigitFromRight);
+}
+
 const expandedForm = (input) => {
 	const arrayOfDigits = ('' + input).split('');
-
 	let resultString = '';
 
 	for (let i = 0; i < arrayOfDigits.length; i++) {
+		const digit = arrayOfDigits[i];
 		const positionOfDigitFromRight = arrayOfDigits.length - 1 - i;
 
-		resultString += arrayOfDigits[i] * Math.pow(10, positionOfDigitFromRight);
+		resultString += expandedFormOfSingleDigit(digit, positionOfDigitFromRight);
 
 		if (shouldAddPlusSign(positionOfDigitFromRight)) {
 			resultString += ' + ';
